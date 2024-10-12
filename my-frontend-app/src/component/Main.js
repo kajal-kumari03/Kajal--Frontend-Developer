9ab694af479e1e991e74b266ecfb24a96c93d86b
kajal-kumari03 <kajalkumari1235669@gmail.com>
2024-10-12 14:47:30 +0530

    Structure below Roadmaps

---
+++ my-frontend-app/src/component/Main.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import './Main.css';
+
+const Navbar = () => {
+  return (
+  <div>
+    {/* -------------------------------------------------first div--------------------------------------- */}
+    <div className="firstdiv">
+     <div className='Nav'>
+        <div className='logo'><img src='./logo.jpg'></img><span className='logoname'>EthAi</span> </div>
+        <div></div>
+        <div></div>
+     </div>
+     <div className='mid1'>
+
+     </div>
+     <div className='footer1'>
+
+     </div>
+    </div>
+     {/* -------------------------------------------Road Map------------------------------------------------ */}
+   <div className='roadmap'>
+    <div className='kickingoff'>
+         <div className='kl '> 
+          <h6>Kicking Off</h6>
+          <p>Launch of EthAi</p>
+          <p>Development of Core AI Aganets</p>
+          <p>User Onboarding Campaign</p>
+          <p>Community Engagement Initiatives</p>
+         </div>
+         <div className='kr'><video src='https://s3-figma-videos-production-sig.figma.com/video/1304101392342992653/TEAM/037f/7f94/-dbe5-4e84-a0d0-34a0193e07d6?Expires=1729468800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=eM4i89cT0EJLQ49khHxjzPae44XSB9w63wQa6rORhPrzphN3BtxunBXO--s6tcRH895EIDtekvf1VqGNcGO4U7njl1k5j5JquXD7LAZGLiEasFuYc-Q1RyO5TKQmVX5GSbRuDDzLdoyG7k~-KAazGsk~Nc7YRTNK5Dz69BR5uP5e5DLjmaHXf1AwzBOI6YB70L9U3~iCCrJIgJ5cwoveC6NonfYVhQCBTpSqJms1XtQjoo-ZJm7-2mk4TktMwJULpKDqoyzQZU7z87xfjVivChwW4VxRcR8LwBZdwdwuckG5~9KF7LFZIv-sZ7yddWRnmatbsYXBEv28h1RmPC-HxA__'></video></div>
+    </div>
+   <div className='biggerinsights'>
+     <div className='bl'><video src='https://s3-figma-videos-production-sig.figma.com/video/1304101392342992653/TEAM/cb86/a228/-0cff-4aa8-9d9a-c49552d1766c?Expires=1729468800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ng05S9ao8zXQU2qAK7d0Q2SHfp7HPqm2flM0GxxNEZkJ~gG7QzPOJAPCDB6IfXmV49XHXpmzojD1zAdqdWCvAp1CW27Zu4AMWxPVRb~c4hxqSi6aCjeqUA8WbO-skAT8w1bm9NC3k-PbWOACCVaPg1SR4tqhRycuZ07sNXfyr6eTTRvgIq8mcjIxW9I9hoUYkds-eHp9VjsAK1smsn90ozZ8yqgqQlNnrvFHpCnFwr-izY6bHffdUdOchxSNQvfWCb7L2LXQhwYq~rIvUv8y4H2b5zZQ8176p4~A6ULgGsGybPe-rSzhO5Bt1dGA3u~ijHnOBVTN~qA9UYKAzAu1gg__'></video></div>
+   <div className='br'>
+    <h6>Bigger Insight</h6>
+    <p>Introduction of advance AI Agents</p>
+    <p>Atrategic Partnerships</p>
+    <p>User Interface Optimizetion</p>
+    <p>Comprehensive Marketing Campaign</p>
+   </div>
+   </div>
+   <div className='fullpower'>
+      <div>
+        <h6>Full Power</h6>
+        <p>Launch of AI-Powered Tools</p>
+        <p>Expansion of AI Agents</p>
+        <p>API Integration for Data Access</p>
+      </div>
+      <div><video src='https://s3-figma-videos-production-sig.figma.com/video/1304101392342992653/TEAM/86e0/aff1/-c5e4-4b3f-b0ee-214e16d683f4?Expires=1729468800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aOQHLlqT3DJileOvM3lAL2gABNJS4CR5akXA57rACCUr-PecfKVN9~FSgnpkC-M5~LhN6ntWFFWybnZk600HcOO7MQkm3B5VwXK6euAoo85AXvB6uVkzdzEBOEXQkicU4CyoPoGpEkS3XyFdYb5i~6~LE~zwan-Dg-pHy5oQ01jSUk37aKFq8FsAUexXVt2wph-1uiXcEgSvv3WQJF7JreLgh90uibzTkoilsFJ5v1i8D30zmXeyqJKY00kSsmrRRD86r3aqEfNHCGYfBgIXn2B7LaVd4MZj5Co5FNfuTcebvAhaMOkBMaybC80EfYjWVj7lE4NvHGI-1qtsoj7O0A__'></video></div>
+   </div>
+ </div>
+ {/* --------------------------------------FAQ------------------------------------------------------------------- */}
+  <div className='faq'>
+    <div className='ql'><h6>Frequently Asked Questions</h6></div>
+    <div className='qr'>
+      <p>What is EthAi</p>
+      <p>How can EthAi can help me as a Trader</p>
+      <p>How can I get started with EthAi</p>
+      <p>Who can use EthAi</p>
+    </div>
+  </div>
+  {/* ----------------------------Explore Our dApp------------------------------------ */}
+  <div>
+    <div className='inside'> 
+      <h4>Explore Our dApp</h4>
+      <p>EthAi is an AI-powered dApp designed to hlp traders make Samter, data-driver decisions.By tracking smart money flows, mnitoring ket wallets, and prviding realtime market insignts. EthAi empowers users to stay ahead of trends of traders. The Platforms offers instuitive AI features for asset recommendations market analyis and personaized crypto QAS, amking it the ultimated tool for both novice and experiences traders</p>
+      <button>Open dApp</button>
+    </div>
+  </div>
+  {/* -----------------------------------footer------------------------------------------------------ */}
+   <div className='footer'>
+      <div className='footlogo'></div>
+      <div className='socail'></div>
+      <div className='text'><h3>"Designment for traders of today, just like you."</h3></div>
+      <div><label>What's your work email?</label><button>Get Started</button></div>
+   </div>
+
+
+  </div>
+  )
+}
+
+export default Navbar;
